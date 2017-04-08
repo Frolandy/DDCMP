a13e69eb0dd5fd084235066badbeef9a9e5d3163
Andrey <frolandy@mail.ru>
2017-04-09 02:58:06 +0300

    Update Write-Read.c

--- DDCMP/Src/ddcmp/Write-Read.c
+++ DDCMP/Src/ddcmp/Write-Read.c
@@ -1,18 +1,19 @@
 #include "Write-Read.h"
 
-
 void write_on_port(UART_HandleTypeDef *Uart, int size){
 	for(int i=0; i<size; i++){
 		USART_PutChar(Uart,send_buffer[i]);
 	}
 }
-int read_on_port(UART_HandleTypeDef *Uart){
-	int size = 0;
-		do{
-			receive_buffer[size] = USART_GetChar(Uart);
-			size ++;
-			for(int i=0; i<10000; i++){
+int read_on_port(UART_HandleTypeDef *Uart, int byte){
+	int _is_readed = 0;
+
+			if(USART_GetRxCount(Uart) > 0){
+			receive_buffer[_received_b++] = USART_GetChar(Uart);
+			if (_received_b == byte) {
+				_is_readed = 1;
+				_received_b = 0;
 			}
-			}while(USART_GetRxCount(Uart) > 0);
-		return size;
+		}
+			return _is_readed;
 }
