dc820aa2edc0a3f9f50e9dd9ac3f7d6c9c9b8b1d
Andrey <frolandy@mail.ru>
2017-04-09 02:59:40 +0300

    Update data_message.c

--- DDCMP/Src/ddcmp/data_message.c
+++ DDCMP/Src/ddcmp/data_message.c
@@ -11,43 +11,40 @@ void set_data_header(int size){    //Установка заголовка
 }
 
 int send_way(UART_HandleTypeDef *Uart){  //Отправка Who are u
-  set_data_header(16);
+	set_data_header(16);
 
-  send_buffer[8] = 0x77;
-  send_buffer[9] = 0xE0;
-  send_buffer[10] = 0x00;
-  send_buffer[23] = accesstype;
+	send_buffer[8] = 0x77;
+	send_buffer[9] = 0xE0;
+	send_buffer[10] = 0x00;
+	send_buffer[23] = accesstype;
 
-  memcpy(&send_buffer[11],securitycode,2);
-  memcpy(&send_buffer[13],passcode,2);
-  memcpy(&send_buffer[21],iruserid,2);
+	memcpy(&send_buffer[11],securitycode,2);
+	memcpy(&send_buffer[13],passcode,2);
+	memcpy(&send_buffer[21],iruserid,2);
 
-  set_crc(&send_buffer[8],16);
+	set_crc(&send_buffer[8],16);
 
-  sent = WAY;
-  waiting = ACK;
+	sent_control(WAY);
 
-  return resend(26,8,0, Uart);
+	return resend(26, Uart);
 }
 
 int send_read_data(UART_HandleTypeDef *Uart){ //Отправка READ DATA
-    set_data_header(9);
+	  set_data_header(9);
 
-    send_buffer[8] = 0x77;
-    send_buffer[9] = 0xe2;
-    send_buffer[10] = 0x00;
-    send_buffer[11] = list_name;
-    send_buffer[12] = 1;
-    send_buffer[13] = 0;
-    send_buffer[14] = 0;
-    send_buffer[15] = 0;
-    send_buffer[16] = 0;
+	  send_buffer[8] = 0x77;
+	  send_buffer[9] = 0xe2;
+	  send_buffer[10] = 0x00;
+	  send_buffer[11] = list_name;
+	  send_buffer[12] = 1;
+	  send_buffer[13] = 0;
+	  send_buffer[14] = 0;
+	  send_buffer[15] = 0;
+	  send_buffer[16] = 0;
 
-    setcrc(&send_buffer[8],9);
+	  set_crc(&send_buffer[8],9);
 
-    sent = READ_DATA;
-    waiting = ACK;
+	  sent_control(READ_DATA);
 
-    return(resend(19,8,0, Uart));
+	  return(resend(19, Uart));
 }
-
