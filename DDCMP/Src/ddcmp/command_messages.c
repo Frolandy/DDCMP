866a378640e72ed71d6a22660e53dc839ede75e0
Andrey <frolandy@mail.ru>
2017-04-09 02:58:44 +0300

    Update command_messages.c

--- DDCMP/Src/ddcmp/command_messages.c
+++ DDCMP/Src/ddcmp/command_messages.c
@@ -1,28 +1,30 @@
 #include "command_messages.h"
+#include "USART.h"
 
 int send_start(UART_HandleTypeDef *Uart) { //Отправить START
   memcpy(send_buffer,msgstart,6);
   set_crc(send_buffer, 6);
-  sent = START;
+  sent_control(START);
   send_cnt = 1;
   receive_cnt = 1;
-  return(resend(8, Uart));
+  bytes(8);
+  return resend(8, Uart);
 }
 
 int send_ack(UART_HandleTypeDef *Uart){   //Отправить АСК
   msgack[3] = receive_cnt++;
   memcpy(send_buffer,msgack,6);
   set_crc(send_buffer, 6);
-  sent = ACK;
+  sent_control(ACK);
   send_cnt++;
   return(resend(8, Uart));
 }
 
 int send_stack(UART_HandleTypeDef *Uart){ //Отправить STACK
-   memcpy(send_buffer,msgstack,6);
-   set_crc(send_buffer, 6);
-   sent = STACK;
-   return(resend(8, Uart));
+	 memcpy(send_buffer,msgstack,6);
+	 set_crc(send_buffer, 6);
+	 sent_control(STACK);
+	 return(resend(8, Uart));
 }
 
 int send_nack(UART_HandleTypeDef *Uart){  //Отправить NACK
@@ -30,7 +32,7 @@ int send_nack(UART_HandleTypeDef *Uart){  //Отправить NACK
   msgnack[3] = receive_cnt - 1;
   memcpy(send_buffer, msgnack, 6);
   set_crc(send_buffer, 6);
-  sent = NACK;
+  sent_control(NACK);
   send_cnt++;
   return(resend(8, Uart));
 }
