f972ae97e872cbd717665c7adbf9032108a41e1e
Andrey <frolandy@mail.ru>
2017-04-09 03:03:22 +0300

    Update processing.h

--- DDCMP/Inc/ddcmp/processing.h
+++ DDCMP/Inc/ddcmp/processing.h
@@ -10,7 +10,23 @@
 
 #include "data_message.h"
 #include "ddcmp_session.h"
+#include "USART.h"
 
-int processing(UART_HandleTypeDef *Uart);
+static int receive_message_type = 0;
+static int receive_message = 0;
+
+static unsigned char dataflag;
+
+static int sent = 0;
+static long reclaenge;
+static long restreclaenge;
+static int byteread;
+
+void sent_control(int msg);
+void parsing_message();
+void answer(UART_HandleTypeDef *Uart);
+
+void processing(UART_HandleTypeDef *Uart);
+
 
 #endif /* INC_PROCESSING_H_ */
