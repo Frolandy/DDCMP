19cd048eefa899ac5f6204066a83397a59f1bdf4
Andrey <frolandy@mail.ru>
2017-04-09 03:05:30 +0300

    Update ddcmp_session.h

--- DDCMP/Inc/ddcmp/ddcmp_session.h
+++ DDCMP/Inc/ddcmp/ddcmp_session.h
@@ -1,4 +1,5 @@
-/* ddcmp_session.h
+/*
+ * ddcmp_session.h
  *
  *  Created on: 11 мар. 2017 г.
  *      Author: frol
@@ -10,11 +11,39 @@
 
 #include "USART.h"
 #include "board.h"
-#include "resources.h"
 #include "Write-Read.h"
 #include "command_messages.h"
 #include <string.h>
 
+
+#define BUFFER_SIZE     260
+#define IRREADANZ       256
+#define IRDATAFIRST     0x01
+#define IRDATAAGAIN     0x02
+#define IRDATACOMPLETE  0x04
+
+#define CONTROL_MESSAGES 		1
+#define DATA_COMMAND_MESSAGES 	2
+#define DATA_MESSAGE_HEADER 	9
+#define DATA_COMMAND_RESPONSES 	4
+#define DATA_BLOCKS 			5
+
+#define START 					1
+#define STACK 					2
+#define ACK 					3
+#define NACK 					4
+#define WAY 					5
+#define WAY_RESP 				6
+#define READ_DATA 				7
+#define READ_DATA_RESP 			8
+
+#define OK						1
+#define ERROR					0
+
+char send_buffer[BUFFER_SIZE];    //Буфер передачи
+unsigned char receive_buffer[BUFFER_SIZE];  //Буфер получения
+
+static unsigned int crc;
 
 int check_crc(unsigned char *buf, int cnt);
 void set_crc(char *buf, int cnt);
